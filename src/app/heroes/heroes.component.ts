130249515a1bff541ea75b0e8029773482d2bc74
Roman Nemchinov <Roman_Nemchinov@epam.com>
2018-05-18 15:23:31 +0300

    games with buttons

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,16 +9,22 @@ import { HEROES } from '../mock-Herous';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-
   selectedHero: Hero;
 
-
   constructor() { }
-
   ngOnInit() {
   }
 
   onSelect(hero: Hero): void {
+    console.log("selectedHero: ", hero);
     this.selectedHero = hero;
+  }
+
+  onClicked(): void {
+    alert("Hello, Bro");
+  }
+
+  onConsoleLog(str: string): void {
+    console.log("I'am working", str);
   }
 }
